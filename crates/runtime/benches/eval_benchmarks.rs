570e227abaad7e0250c168986f058aba7eb8ec95
magicfoodhand <mitch@inapinch.io>
2025-01-21 14:44:47 -0700

    profile.bench, dont include default modules for runtime benchmark

--- crates/runtime/benches/eval_benchmarks.rs
+++ crates/runtime/benches/eval_benchmarks.rs
@@ -1,16 +1,20 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use rigz_runtime::runtime::eval;
+use rigz_runtime::Runtime;
+
+// run these benchmarks without default modules
 
 fn expressions(c: &mut Criterion) {
     c.bench_function("2 + 2", |b| {
         b.iter(|| {
-            let _ = eval("2 + 2".to_string()).expect("Run Failed");
+            let mut runtime = Runtime::default();
+            let _ = runtime.eval("2 + 2".to_string()).expect("Run Failed");
         })
     });
 
     c.bench_function("factorial(10)", |b| {
         b.iter(|| {
-            let _ = eval(
+            let mut runtime = Runtime::default();
+            let _ = runtime.eval(
                 r#"
                 fn factorial(n)
                     if n <= 1
