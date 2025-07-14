8bafd281b8abc779eea7d768d35d881515ca9414
magicfoodhand <mitch@inapinch.io>
2025-07-13 23:38:07 -0600

    implement derive for enum

--- crates/core/src/derive/enum_value.rs
+++ crates/core/src/derive/enum_value.rs
@@ -1,9 +1,17 @@
 use crate::EnumDeclaration;
 use proc_macro2::TokenStream;
-use quote::ToTokens;
+use quote::{quote, ToTokens};
+use crate::derive::{csv_tuple_vec, csv_vec};
 
 impl ToTokens for EnumDeclaration {
     fn to_tokens(&self, tokens: &mut TokenStream) {
-        todo!()
+        let EnumDeclaration { name, variants } = self;
+        let vars = csv_tuple_vec(variants);
+        tokens.extend(quote! {
+            EnumDeclaration {
+                name: #name.to_string(),
+                variants: #vars,
+            }
+        })
     }
 }
