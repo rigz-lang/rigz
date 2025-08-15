209c6171d01733a80af262a768a7a7797481d24a
magicfoodhand <mitch@inapinch.io>
2025-08-14 20:48:05 -0600

    create UUID object

--- crates/runtime/src/modules/uuid.rs
+++ crates/runtime/src/modules/uuid.rs
@@ -1,27 +1,80 @@
+use std::cell::RefCell;
+use std::ops::Deref;
+use std::rc::Rc;
+use log::warn;
 use rigz_ast::*;
-use rigz_ast_derive::derive_module;
+use rigz_ast_derive::{derive_module, derive_object};
 use rigz_core::*;
 use uuid::Uuid;
 
+derive_object! {
+    "UUID",
+    struct UUID {
+        uuid: Uuid
+    },
+    r#"object UUID
+        Self(value: String? = none)
+
+        fn Self.to_s -> String
+    end"#
+}
+
+impl From<Uuid> for UUID {
+    fn from(value: Uuid) -> Self {
+        Self {
+            uuid: value
+        }
+    }
+}
+
+impl AsPrimitive<ObjectValue, Rc<RefCell<ObjectValue>>> for UUID {}
+
+impl UUIDObject for UUID {
+    fn to_s(&self) -> String {
+        self.uuid.to_string()
+    }
+}
+
+impl CreateObject for UUID {
+    fn create(args: RigzArgs) -> Result<Self, VMError>
+    where
+        Self: Sized
+    {
+        if args.is_empty() {
+            return Ok(UUID::default())
+        }
+
+        if args.len() > 1 {
+            warn!("Ignoring additional args for UUID - {:?}", &args.0[1..]);
+        }
+
+        let first = args.0[0].borrow();
+        if let ObjectValue::Primitive(PrimitiveValue::String(s)) = first.deref() {
+            match Uuid::parse_str(s) {
+                Ok(u) => Ok(u.into()),
+                Err(e) => Err(VMError::runtime(format!("Cannot create UUID from {s}, {e:?}")))
+            }
+        } else {
+            Err(VMError::UnsupportedOperation(format!("Cannot create UUID from {first}")))
+        }
+    }
+}
+
 derive_module! {
+    [UUID],
     r#"
 trait UUID
-    fn v4 -> String
-    fn from(input: String) -> String!
+    fn v4 -> UUID::UUID
+
+    fn create(input: String) -> UUID::UUID!
+        UUID::UUID.new input
+    end
 end
 "#
 }
 
-// todo once object exists use that instead of strings
 impl RigzUUID for UUIDModule {
-    fn v4(&self) -> String {
-        Uuid::new_v4().to_string()
-    }
-
-    fn from(&self, input: String) -> Result<String, VMError> {
-        match Uuid::try_parse(input.as_str()) {
-            Ok(s) => Ok(s.to_string()),
-            Err(e) => Err(VMError::runtime(format!("Failed to parse uuid: {e}"))),
-        }
+    fn v4(&self) -> ObjectValue {
+        ObjectValue::Object(Box::new(Into::<UUID>::into(Uuid::new_v4())))
     }
 }
